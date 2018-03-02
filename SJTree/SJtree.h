dbcc0016d460cd2c3384e0d5bce108ffa0fadd62
sergei <isara.isara8@gmail.com>
2018-03-02 22:07:14 +0200

    dev1

--- SJTree/SJtree.h
+++ SJTree/SJtree.h
@@ -1,17 +1,22 @@
 #pragma once
 
+using namespace std;
 
 namespace Tree
 {
+
+
 	template <typename keyType, class dataType>
 	struct searchNode
 	{
 		keyType  key;  // standart type key so we can doo operations between them like <,>,==
 		dataType data; // so any type data can be stored 
-		searchNode< keyType, dataType > *pLeft = nullptr;  // left sub-tree
-		searchNode< keyType, dataType > *pRight = nullptr; // right sub-tree
+		searchNode<keyType, dataType> *pLeft = nullptr;  // left sub-tree
+		searchNode<keyType, dataType> *pRight = nullptr; // right sub-tree
 	};
 
+#define sTree searchTree<keyType, dataType>
+#define sNode searchNode<keyType, dataType>
 
 	template <typename keyType, class dataType>
 	class searchTree
@@ -20,31 +25,76 @@ namespace Tree
 		searchTree();
 		~searchTree();
 
-		void add(dataType addElement);
+		void add(keyType addKey,dataType addElement); // interface to add element
 	private:
 		//Data//
-		searchNode< keyType, dataType > *pRoot = NULL;
+		sNode *pRoot = nullptr;
 
 		//Methods//
-
-
+		bool cmp(keyType addKey, sNode *subTree);   // 0 == left, 1 == right, subTree is a sub-tree on chosen level
+		void recAdd(keyType addKey, dataType addElement, sNode *subTree);// recursive adding of element to the sub-tree 
 	};
 
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	template <typename keyType, class dataType>
-	searchTree::searchTree()
+	searchTree<keyType, dataType>::searchTree()
 	{
 
 	}
 
 	template <typename keyType, class dataType>
-	searchTree::~searchTree()
+	searchTree<keyType, dataType>::~searchTree()
 	{
 
 	}
 
 	template <typename keyType, class dataType>
-	void searchTree::add(dataType addElementd)
+	void searchTree<keyType, dataType>::add(keyType addKey, dataType addElement)
 	{
+		if (this->pRoot == nullptr)
+		{
+			sNode *newElem = new sNode();
+			newElem->data = addElement;
+			newElem->key = addKey;
+			this->pRoot = newElem;
+			return;
+		}
+		recAdd(addKey, addElement, this->pRoot);
 
 	}
+
+	template <typename keyType, class dataType>
+	bool searchTree<keyType, dataType>::cmp(keyType addKey, sNode *subTree)
+	{
+		return (addKey > subTree->key);
+	}
+
+	template <typename keyType, class dataType>
+	void searchTree<keyType, dataType>::recAdd(keyType addKey, dataType addElement, sNode *subTree)
+	{
+		if (cmp(addKey, subTree))
+		{
+			if (subTree->pRight == nullptr)
+			{
+				subTree->pRight = new sNode();
+				subTree->pRight->key = addKey;
+				subTree->pRight->data = addElement;
+				return;
+			}
+			recAdd(addKey, addElement, subTree->pRight);
+			return;
+		}
+		if (subTree->pLeft == nullptr)
+		{
+			subTree->pLeft = new sNode();
+			subTree->pLeft->key = addKey;
+			subTree->pLeft->data = addElement;
+			return;
+		}
+		recAdd(addKey, addElement, subTree->pLeft);
+	}
+
+
 }
--- SJTree/main.cpp
+++ SJTree/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "SJtree.h"
 
-using namespace std;
+using namespace Tree;
 
 int main()
 {
+	searchTree<int, int> tree;
+	tree.add(1, 1);
+	tree.add(2, 32);
+	tree.add(1, 22);
+	tree.add(33, 2);
 
 	system("pause");
 	return 0;
